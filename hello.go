6198a7099ba25ec036e7126a8f8bed31ca5171cd
RenanLMenezes <renanlmenezes2003@gmail.com>
2023-04-03 22:13:14 -0300

    Variables

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	//variables
+	// if u don't use a variable Go don't compile
+	// u can declare without the type
+	// u can u := instead var and without the type
+	name := "Renan"   //var name string
+	var age = 20      // if u don't initialize the variable the value is 0 ou null | var age int
+	var version = 0.2 // var version float32 || float64
+
+	//print
+	fmt.Println("Hello World! And", name, "your age is", age)
+	fmt.Println("This program is on the version", version)
+	fmt.Println("Type of variable version is", reflect.TypeOf(version)) // show the type of the variable
 }
