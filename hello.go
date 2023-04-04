12f86576a18a722e30762fa0c1b8caab47b02b89
Renan Menezes <renanlmenezes2003@gmail.com>
2023-04-04 08:54:05 -0300

    Functions

--- hello.go
+++ hello.go
@@ -2,34 +2,14 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
 func main() {
-	//variables
-	// if u don't use a variable Go don't compile
-	// u can declare without the type
-	// u can u := instead var and without the type
-	name := "Renan" //var name string
-	//var age = 20      // if u don't initialize the variable the value is 0 ou null | var age int
-	var version = 0.2 // var version float32 || float64
 
-	//print
-	fmt.Println("Hello World! And", name)
-	fmt.Println("This program is on the version", version)
-	fmt.Println("Type of variable version is", reflect.TypeOf(version)) // show the type of the variable
+	showIntrodution()
 
-	fmt.Println("\n1 - Start monitoring")
-	fmt.Println("2 - View the logs")
-	fmt.Println("0 - Exit")
-
-	fmt.Print("\nChoose your option: ")
-	//func to receive input
-	var command int
-	fmt.Scan(&command) // or Scanf("%d", &commando)
-
-	fmt.Println("The command is", command)
-	//fmt.Println("The address of command is", &command) //var address
+	showMenu()
 
 	// if command == 1 {
 	// 	fmt.Println("Monitoring...")
@@ -41,14 +21,50 @@ func main() {
 	// 	fmt.Println("Choose a valid option")
 	// }
 
-	switch command {
+	//comando := receiveCommand()
+
+	switch receiveCommand() {
 	case 1:
 		fmt.Println("Monitoring...")
 	case 2:
 		fmt.Println("Showing Logs...")
 	case 0:
-		fmt.Println("Choose a valid option")
+		fmt.Println("Bye bye...")
+		os.Exit(0)
 	default:
 		fmt.Println("Choose a valid option")
+		os.Exit(-1)
 	}
+}
+
+func showIntrodution() {
+	//variables
+	// if u don't use a variable Go don't compile
+	// u can declare without the type
+	// u can u := instead var and without the type
+	name := "Renan" //var name string
+	//var age = 20      // if u don't initialize the variable the value is 0 ou null | var age int
+	var version = 0.2 // var version float32 || float64
+
+	//print
+	fmt.Println("Hello World! And", name)
+	fmt.Println("This program is on the version", version)
+}
+
+func showMenu() {
+	fmt.Println("\n1 - Start monitoring")
+	fmt.Println("2 - View the logs")
+	fmt.Println("0 - Exit")
+}
+
+func receiveCommand() int {
+	fmt.Print("\nChoose your option: ")
+	//func to receive input
+	var command int
+	fmt.Scan(&command) // or Scanf("%d", &commando)
+
+	fmt.Println("The command is", command)
+	//fmt.Println("The address of command is", &command) //var address
+
+	return command
 }
